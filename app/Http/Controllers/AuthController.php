e7e5207a3f5d820992af7945e0f375d7dbc2a3a9
Miljanovic <zteblade3.dm@gmail.com>
2022-05-16 15:17:12 +0200

    readme file updated

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -68,7 +68,7 @@ class AuthController extends Controller
     public function logout()
     {
         request()->session()->forget('user_data');
-        request()->session()->flash('message', 'Goodby.');
+        request()->session()->flash('message', 'Goodbye.');
 
         return redirect()->route('home.index');
     }
